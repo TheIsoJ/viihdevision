fbbf2a9f3c0001ef53e0db8010681ae7e4091146
Jesse Keskelä <monkeydie_@hotmail.com>
2023-05-21 17:49:16 +0300

    update src/pages/haku.tsx

--- src/pages/haku.tsx
+++ src/pages/haku.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import { FormEvent, useState } from "reacr/useSearchResultsFetch"
+import { FormEvent, useState } from "react"
 import Link from "next/link"
 import { DotPulse } from "@uiball/loaders"
 
