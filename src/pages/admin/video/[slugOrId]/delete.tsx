6bce200ebf760b5131af2d2982d7992cab6fff2c
Jesse Keskelä <monkeydie_@hotmail.com>
2023-05-21 17:26:49 +0300

    Korjaus.

--- src/pages/admin/video/[slugOrId]/delete.tsx
+++ src/pages/admin/video/[slugOrId]/delete.tsx
@@ -1,30 +1,27 @@
 import Head from "next/head"
 import Header from "../../../../components/Header"
 import { useRouter } from "next/router"
-import { useAdminPodcastDelete } from "../../../../hooks/admin/podcasts/useAdminPodcastDelete"
 import Footer from "../../../../components/Footer"
 
 const DeleteRickroll = () => {
     const router = useRouter()
     const { slugOrId: id } = router.query
 
-    async function onRickrollDelete(id: string) {
-      try {
-        useAdminPodcastDelete<void>(id).then(() => {
-          router.replace("/admin")
-        })
-      } catch (error) {
-        console.log(error)
-      }
-    }
+    // async function onRickrollDelete(id: string) {
+    //   try {
+    //     useAdminPodcastDelete<void>(id).then(() => {
+    //       router.replace("/admin")
+    //     })
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
 
   return (
     <>
       <Head>
         <title>Poista rickroll - Rickrolls</title>
       </Head>
-
-      <Header sticky themeColor="bg-white" />
 
       <div className="flex items-center fade-semifast justify-center space-y-8 space-x-2 bg-white text-black p-5">
         <div>
@@ -42,7 +39,7 @@ const DeleteRickroll = () => {
           Ei
         </button>
         <button
-          onClick={() => onRickrollDelete(id as string)}
+          // onClick={() => onRickrollDelete(id as string)}
           className="flex items-center uppercase justify-center bg-[#002f6c] text-white hover:shadow-md hover:shadow-gray-500 rounded-full font-[Poppins] font-bold px-12 py-[0.75rem] transition-all duration-500 ease-in-out hover:bg-red-600"
           type="button"
         >
@@ -50,7 +47,7 @@ const DeleteRickroll = () => {
         </button>
       </div>
 
-      <Footer themeColor="bg-white" />
+      <Footer />
     </>
   )
 }
